2607b43f97ac14b6d0a5bedd9029138277aed177
mutai-gilbert <mutaigilbert85@gmail.com>
2023-11-18 20:51:25 +0300

    Add the app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import routes from "./routes/routes.js"
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/app/index.html");
+});
+
+app.use("/video", routes);
+
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
