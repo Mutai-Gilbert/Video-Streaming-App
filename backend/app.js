29d6f8f9fdcaf149d81b49d5182a63d0a5266d88
mutai-gilbert <mutaigilbert85@gmail.com>
2023-11-22 22:14:53 +0300

    Add new updates to the app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import routes from "./routes/routes.js"
+import routes from "../routes/routes.js"
 
 const app = express();
 app.use(cors());
---
+++ frontend/vue-project/src/components/VideoUpload.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="file" @change="uploadVideo">
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    uploadVideo(event){
+      const file = event.target.files[0];
+      const formData = new FormData();
+
+      formData.append('video', file);
+      this.$axios.post('')
+    }
+  }
+}
+</script>
